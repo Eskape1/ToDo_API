a8b317f4730ad07cd4ef9f3423610ab050d4d5a9
Eskape <danyagamievo@gmail.com>
2025-07-04 11:34:53 +0200

    added filter to tasks.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Query, HTTPException
 from models import Task, TaskModel
 from storage import add_task, get_tasks, put_task, remove_task
+from typing import List, Annotated
+
+from storage import tasks
 
 app = FastAPI()
 
@@ -14,12 +16,26 @@ async def create_task(task: TaskModel):
 
 
 @app.get("/tasks", response_model=list[Task])
-def read_tasks():
-    return get_tasks()
+def read_tasks(tag: str | None = None, 
+               sort: Annotated[str | None, Query(description="Sort by 'newest' or 'latest'")] = None, 
+               limit: Annotated[int | None, Query(description='Amount of output data at a time', gt=0)] = None):
+    filtered: List[Task] = tasks
+    if tag:
+        filtered = [n for n in filtered if tag in n.tags]
+    if sort == 'newest':
+        filtered.sort(key=lambda n: n.created, reverse=True)
+    elif sort == 'latest':
+        filtered.sort(key=lambda n: n.created)
+    elif sort:
+        raise HTTPException(status_code=400, detail='invalid request')
+    if limit:
+        filtered = filtered[:limit]
+        
+    return filtered
 
 @app.put("/tasks/change/{task_id}")
 def change_task(task_id: int, task: TaskModel):
-    return put_task(task_id, task.title, task.txt)
+    return put_task(task_id, task.title, task.txt, task.tags)
 
 @app.delete("/tasks/delete/{task_id}")
 def delete_task(task_id: int):
