f2f42eb51014014ca59ca041ca6a6e916f540ef3
Eskape <danyagamievo@gmail.com>
2025-07-04 11:38:51 +0200

    put func begin to change date, added tags.

--- storage.py
+++ storage.py
@@ -1,7 +1,7 @@
 from models import Task, TaskModel
 from datetime import datetime
 from fastapi import HTTPException
-
+from typing import List
 
 tasks = []
 ln = len(tasks)
@@ -14,18 +14,20 @@ def get_tasks() -> list[Task]:
 #post
 def add_task(task: TaskModel ) -> Task:
     global global_id
-    n_task = Task(id=global_id, title=task.title, txt=task.txt, created=datetime.now())
+    n_task = Task(id=global_id, title=task.title, txt=task.txt, created=datetime.now(), tags=task.tags)
     tasks.append(n_task)
     global_id += 1
     return n_task
 
 #put
-def put_task(task_id: int, n_title, n_txt) -> Task: 
+def put_task(task_id: int, n_title: str, n_txt: str, n_tags: List[str] | None = None) -> Task: 
     for i, el in enumerate(tasks):
         if el.id == task_id:
             task: Task = tasks[i]
             task.title = n_title
             task.txt = n_txt
+            task.tags = n_tags
+            task.created = datetime.now()
             return task 
     raise HTTPException(status_code=404, detail="Task not found")
 
