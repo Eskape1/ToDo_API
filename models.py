3045c7a15a02457b659d83c210672b733782849d
Eskape <danyagamievo@gmail.com>
2025-07-04 11:35:35 +0200

    added tags to existed models

--- models.py
+++ models.py
@@ -1,13 +1,15 @@
 from pydantic import BaseModel
 from datetime import datetime
-#from typing import Optional, List
+from typing import List
 
 class Task(BaseModel):
     id: int
     title: str
     txt: str
     created: datetime
+    tags: List[str] | None = []
 
 class TaskModel(BaseModel):
     title: str = "Your title"
     txt: str = "lorem ipsum"
+    tags: List[str] | None = ['lorem', 'ipsum']
