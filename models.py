66190bffa11333327dfdd00ae2b4338f60667845
Eskape <danyagamievo@gmail.com>
2025-07-07 14:11:57 +0200

    imported database. created new model. changed structure and renamed classes. inserted fields

--- models.py
+++ models.py
@@ -1,15 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 from typing import List
+from sqlalchemy import Column, String, Integer, DateTime
 
-class Task(BaseModel):
-    id: int
-    title: str
-    txt: str
-    created: datetime
-    tags: List[str] | None = []
+from database import Base
 
-class TaskModel(BaseModel):
-    title: str = "Your title"
-    txt: str = "lorem ipsum"
-    tags: List[str] | None = ['lorem', 'ipsum']
+#model which user fills
+class TaskIn(BaseModel):
+    txt: str = Field()
+    tags: List[str] | None =Field(default_factory=list)
+
+#how our Schema look out
+class TaskOut(TaskIn):
+    id: int = Field()
+    created: datetime = Field()
+    class Config:
+        from_attributes = True
+
+#our Schema nut in the database
+class Task(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True)
+    txt = Column(String, nullable=False)
+    created = Column(DateTime, default=datetime.now())
+    tags = Column(String, default='')
